e3a75ff0567cb7b59baf7f35efa6ba6b833e5f74
Mariia Haiduchenko <ma.gaiduchenko@gmail.com>
2023-07-28 21:37:09 +0300

    style!: + global styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { App } from './App.tsx'
+import { GlobalStyle } from './assets/styles/global-style.ts'
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.querySelector('#root')!).render(
     <React.StrictMode>
+        <GlobalStyle />
+
         <App />
     </React.StrictMode>
 )
