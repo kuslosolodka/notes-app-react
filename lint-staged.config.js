ff8a0f03fe96643b9e9f2f1d37f212540f3b318d
Mariia Haiduchenko <ma.gaiduchenko@gmail.com>
2023-07-28 21:35:57 +0300

    chore!: * lint-staged scripts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 export default {
     '*.{json,md,scss,html,js}': () => 'npm run lint:prettify',
-    'src/**.*.{ts,tsx}': () => 'npm run lint:js',
-    'src/**.*.{tsx,ts}': () => 'npm run lint:css',
+    'src/**/*.{ts,tsx}': () => 'npm run lint:js',
+    'src/**/*.{tsx,ts}': () => 'npm run lint:css',
 }
