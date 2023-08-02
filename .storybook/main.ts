e0a9a03415286a0b1fbc38dce133090f6d3120cc
Mariia Haiduchenko <ma.gaiduchenko@gmail.com>
2023-08-02 22:22:17 +0300

    style!: + global styling to stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,10 @@ const config: StorybookConfig = {
         '@storybook/addon-essentials',
         '@storybook/addon-onboarding',
         '@storybook/addon-interactions',
-        '@storybook/addon-styling',
+        {
+            name: '@storybook/addon-styling',
+            options: {},
+        },
     ],
     framework: {
         name: '@storybook/react-vite',
